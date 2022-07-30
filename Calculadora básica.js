5725f5ee266ccf6b6afc55a3d44e6f81f9a2889b
Valmir Júnior <61609240+Valmir-unicap@users.noreply.github.com>
2022-07-30 02:24:08 -0300

    Update Calculadora básica.js

--- Calculadora básica.js
+++ Calculadora básica.js
@@ -8,35 +8,43 @@ console.log("2- Subtração");
 console.log("3- Divisão");
 console.log("4- Multiplicacão");
 console.log("0- Sair do programa");
-var escolha;
+let escolha;
 escolha= prompt("Escolha uma opção acima: ");
-while(escolha<0 && escolha>4){
+while(escolha<0 || escolha>4){
     console.log("Opção inválida");
     escolha= prompt("Escolha uma opção acima: ");
 }
 if(escolha==1){
-    var numero1, numero2, soma;
+    let numero1, numero2;
     numero1= prompt("Digite o primeiro número: ");
     numero2= prompt("Digite o segundo número: ");
-    soma= numero1 + numero2;
-    console.log("O resultado= ",soma);
+    numero1= parseInt(numero1);
+    numero2= parseInt(numero2);
+    const somar= numero1+numero2;
+    console.log("O resultado= ",somar);
 } if(escolha==2){
-    var numero1, numero2, subtracao;
+    let numero1, numero2;
     numero1= prompt("Digite o primeiro número: ");
     numero2= prompt("Digite o segundo número: ");
-    subtracao= numero1 - numero2;
+    numero1= parseInt(numero1);
+    numero2= parseInt(numero2);
+    const subtracao= numero1 - numero2;
     console.log("O resultado= ",subtracao);
 }else if(escolha==3){
-    var numero1, numero2, divisao;
+    let numero1, numero2;
     numero1= prompt("Digite o primeiro número: ");
     numero2= prompt("Digite o segundo número: ");
-    divisao= numero1 / numero2;
+    numero1= parseInt(numero1);
+    numero2= parseInt(numero2);
+    const divisao= numero1 / numero2;
     console.log("O resultado= ",divisao);
 }else if(escolha==4){
-    var numero1, numero2, multiplicacao;
+    let numero1, numero2;
     numero1= prompt("Digite o primeiro número: ");
     numero2= prompt("Digite o segundo número: ");
-    multiplicacao= numero1 * numero2;
+    numero1= parseInt(numero1);
+    numero2= parseInt(numero2);
+    const multiplicacao= numero1 * numero2;
     console.log("O resultado= ",multiplicacao);
 }else{
     console.log("Fim do programa! @Developer Valmir Júnior");
