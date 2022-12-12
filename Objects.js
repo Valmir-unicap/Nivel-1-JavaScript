67f189b7c87a20da725323b06247d9208a8e7934
Valmir Júnior <61609240+Valmir-unicap@users.noreply.github.com>
2022-12-11 22:43:04 -0300

    Create Objects.js

---
+++ Objects.js
@@ -0,0 +1,14 @@
+//Objects
+const livraria = {
+    name: "harry potter",
+    autor: "J.K Rowling",
+    age: 2000,
+    categoria: "ficção",
+    preco: 50.25
+}
+
+console.log(livraria)
+
+console.log("")
+
+console.log(livraria.name)
