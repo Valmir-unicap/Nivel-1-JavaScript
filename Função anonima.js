bb444d0d56d1059f7f59a5aa1ec8dc6615a7a436
Valmir Júnior <61609240+Valmir-unicap@users.noreply.github.com>
2022-12-11 21:52:41 -0300

    Create Função anonima.js

---
+++ Função anonima.js
@@ -0,0 +1,10 @@
+const soma = function(number1,number2){ //funcao anonima
+    let soma = number1 + number2
+    return soma
+}
+
+let number1 , number2
+number1 = 10
+number2 = 15
+
+console.log(`Resultado = ${soma(number1,number2)}`)
