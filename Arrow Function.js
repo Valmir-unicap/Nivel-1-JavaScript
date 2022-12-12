0a437048277c1ab40aa32ef57666c7a2a3232eee
Valmir Júnior <61609240+Valmir-unicap@users.noreply.github.com>
2022-12-11 21:57:16 -0300

    Create Arrow Function.js

---
+++ Arrow Function.js
@@ -0,0 +1,10 @@
+const soma = (number1,number2) => { //arrow function
+    let divisao = number1 / number2
+    return divisao
+}
+
+let number1 , number2
+number1 = 10
+number2 = 5
+
+console.log(`Resultado = ${divisao(number1,number2)}`)
