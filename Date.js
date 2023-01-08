ca081732162a1fbb2540a3a3e2370f39ced80634
Valmir Júnior <61609240+Valmir-unicap@users.noreply.github.com>
2023-01-08 14:49:45 -0300

    Update Date.js

--- Date.js
+++ Date.js
@@ -1,2 +1,25 @@
-let date = new Date("2022-12-10")
-console.log(date)
+let data = new Date();
+
+let dia = data.getDate();
+
+let mes = data.getMonth()+1;
+
+let ano = data.getFullYear();
+
+if (dia < 10){
+  dia= "0" + dia;
+}
+
+if (mes < 10){
+  mes= "0" + mes;
+}
+
+console.log("Dia atual= ",dia);
+console.log("Mês atual= ",mes);
+console.log("Ano atual= ",ano);
+
+//data por extenso
+
+const lastDay = new Date(mes,dia,ano);
+const lastDayDate = data.toLocaleDateString();
+console.log(lastDayDate);
