0cb3699b2830d453f5dc872067bc99e8aead1445
Valmir Junior <61609240+Valmir-unicap@users.noreply.github.com>
2023-03-28 09:04:33 -0300

    Create Array2.js

---
+++ Array2.js
@@ -0,0 +1,12 @@
+console.log("Array")
+let vetor = [1 , 2 , 3 , 4] 
+vetor.unshift(0) //inseri no inicio do vetor
+console.log(vetor)
+vetor.shift(0) //remover no inicio do vetor
+console.log(vetor)
+vetor.push(5) //inseri no final do vetor
+console.log(vetor)
+vetor.pop() //remover no final do vetor
+console.log(vetor)
+vetor.toString() //exibir conteudo do vetor
+vetor.join(" | ")
